d4a19775dfa0904ecbc9bd22c8fda9b7c285c12a
y-06-git <s1f102502889@iniad.org>
2025-06-20 17:41:47 +0900

    'メニューを押したときのぼかし'

--- js/sample.js
+++ js/sample.js
@@ -5,3 +5,10 @@ menuButton.addEventListener('click',() =>{
     menu.classList.toggle('show')
 });
 /*クリック=>showクラス on/off*/
+
+const menuFilter = document.getElementById('menu-button')
+const filter = document.getElementById('page-content')
+
+menuFilter.addEventListener('click',() =>{
+    filter.classList.toggle('filter')
+})
